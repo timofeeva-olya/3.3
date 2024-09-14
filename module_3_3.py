8aff39a8bea3fbf2b194f13bac5cd9314190d597
timofeeva-olya <olya.sinyakova.20.02@mail.ru>
2024-09-14 12:44:34 +0300

    Add files via upload

---
+++ module_3_3.py
@@ -0,0 +1,16 @@
+def print_params(a = 1, b = 'строка', c = True):
+    print(a,b,c)
+
+print_params()
+print_params(b=25)
+print_params(c = [1,2,3])
+
+
+values_list = [42, 'hello', False]
+print_params(*values_list)
+
+values_dict = {'a': 3.14, 'b': 'world', 'c': False}
+print_params(**values_dict)
+
+values_list_2 =  [54.32, 'Строка' ]
+print_params(*values_list_2, 42)
